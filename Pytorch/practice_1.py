73630376706331eeebd5503152d8ae7e5e8bba5b
AlexT11223344 <alex8956123@gmail.com>
2022-07-12 14:41:15 -0500

    change pytorch

--- Pytorch/practice_1.py
+++ Pytorch/practice_1.py
@@ -13,17 +13,3 @@ print(x)
 # create a matrix with values = 0
 x1 = torch.zeros(2, 3)
 print(x1)
-
-# random
-x = torch.rand(2, 2)
-y = torch.rand(2, 2)
-print(x)
-print(y)
-print(x+y)
-print(torch.add(x,y))
-
-
-# Transfer
-a = np.array((1, 2))
-t1 = torch.tensor(a)
-print(t1)
