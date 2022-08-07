647370ccb8de7c40d9a774f12549a7e26dcf6e95
AlexT11223344 <alex8956123@gmail.com>
2022-08-06 22:11:20 -0500

    submit newest code of leetcode

---
+++ DataStructures/Stack_practice_1. Balance Parentheses_2.py
@@ -0,0 +1,40 @@
+"""
+Given parentheses, check if it balanced
+Example: ({})()([]), ([{}]), ({{}})]])(),[[] etc.
+"""
+from Stack import *
+
+s = Stack()
+print(s.items)
+
+
+def par_checker(par_string):
+    balanced = True
+    index = 0
+    while index < len(par_string) and balanced:
+        par = par_string[index]
+        if par in "([{":
+            s.push(par)
+        else:
+            if s.is_empty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not match(top, par):
+                    return False
+        index += 1
+    if balanced and s.is_empty():
+        return True
+    else:
+        return False
+
+def match(open, close):
+    opens = "([{"
+    closes = ")]}"
+    return opens.index(open) == closes.index(close)
+
+
+
+
+print(par_checker("(({([])}))"))
+print(par_checker("((]}"))
